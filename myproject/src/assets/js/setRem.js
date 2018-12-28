d5648accd201aaad6e6a2a3d710b86472c8e061b
carrie <164591357@qq.com>
2018-12-28 16:07:04 +0800

    筛选框

---
+++ myproject/src/assets/js/setRem.js
@@ -0,0 +1,5 @@
+(function(){			
+	var hmtlH = document.documentElement.clientHeight || document.body.clientHeight;
+	var html = document.documentElement;	
+	html.style.fontSize = (hmtlH/7.6) + "px";		
+})()

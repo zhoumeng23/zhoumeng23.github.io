9ca92121d056ddd0da24b32dfdaf1ed276df200b
carrie <164591357@qq.com>
2019-01-05 18:28:47 +0800

    移除美团和index

--- myproject/src/assets/js/setRem.js
+++
@@ -1,5 +0,0 @@
-(function(){			
-	var hmtlH = document.documentElement.clientHeight || document.body.clientHeight;
-	var html = document.documentElement;	
-	html.style.fontSize = (hmtlH/7.6) + "px";		
-})()
